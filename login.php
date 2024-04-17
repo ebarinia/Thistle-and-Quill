3375b71f7c19ab824f426e073e621d4ff76599e3
ebarinia <46579070+ebarinia@users.noreply.github.com>
2024-04-18 00:10:28 +0100

    saving session_id in cookies

--- login.php
+++ login.php
@@ -1,91 +1,59 @@
 <?php
-// session_start();
- 
-// // Check if the user is already logged in, if yes then redirect him to welcome page
-// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-//     header("location: index.php");
-//     exit;
-// }
-
-// Include config file
 include 'connect.php';
-
 include 'create-table.php';
  
-// Define variables and initialize with empty values
 $email = $name = $password = "";
 $email_err = $name_err = $password_err = $login_err = "";
  
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Check if username is empty
     if(empty(trim($_POST["email"]))){
         $email_err = "Please enter your email.";
     } else{
         $email = trim($_POST["email"]);
     }
-    
-    // Check if password is empty
+
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
     
-    // Validate credentials
     if(empty($email_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, email, name, password FROM users WHERE email = ?";
-        
         if($stmt = mysqli_prepare($connect, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
-            
-            // Set parameters
             $param_email = $email;
-            
-            // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Store result
                 mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $email, $name, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
                             session_start();
+
+                            $session_id = session_create_id();
+                            setcookie("session_id", $session_id, time() + (86400 * 30), "/"); // 86400 = 1 day
                             
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["email"] = $email;
-                            $_SESSION["name"] = $name;                            
+                            $_SESSION["name"] = $name;   
                             
-                            // Redirect user to welcome page
                             header("location: index.php");
                         } else{
-                            // Password is not valid, display a generic error message
                             $login_err = "Invalid email or password.";
                         }
                     }
                 } else{
-                    // Username doesn't exist, display a generic error message
                     $login_err = "Invalid email or password.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
-
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
-    
-    // Close connection
     mysqli_close($connect);
 }
 ?>
