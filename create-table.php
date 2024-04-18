7e939b3e5ea5e32e966d15a8b5edd6c8431f3509
ebarinia <46579070+ebarinia@users.noreply.github.com>
2024-04-18 15:22:31 +0100

    fixed the create book table query

--- create-table.php
+++ create-table.php
@@ -1,5 +1,6 @@
 <?php
-  $query = "CREATE TABLE IF NOT EXISTS `users` (
+$query_users = "
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `email` char(50) NOT NULL UNIQUE,
   `name` char(50) NOT NULL,
@@ -8,16 +9,19 @@
   PRIMARY KEY (`id`)
 );";
 
-  $query = "CREATE TABLE IF NOT EXISTS `books` (
-    `id` int unsigned NOT NULL AUTO_INCREMENT,
-    `name` char(255) NOT NULL,
-    `author` char(50) NOT NULL,
-    `publisher` char(50) NOT NULL,
-    `price` float NOT NULL,
-    `synopsis` char(3000) NOT NULL,
-    `image` char(255) NOT NULL,
-    PRIMARY KEY (`id`)
-  );";
+$query_books = "
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  `author` char(50) NOT NULL,
+  `publisher` char(50) NOT NULL,
+  `price` float NOT NULL,
+  `synopsis` text NOT NULL,
+  `image` char(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);";
 
-mysqli_query($connect, $query);
+mysqli_query($connect, $query_users);
+
+mysqli_query($connect, $query_books);
 ?>
